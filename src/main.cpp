632704cee31a7240ab5f8c21273479b5c1365327
Nicolas Graziano <nicolas.graziano@engie.com>
2018-11-09 23:15:47 +0100

    Explicit contructor for OsDeltaTime and constexpr.

--- src/main.cpp
+++ src/main.cpp
@@ -107,6 +107,7 @@ void onEvent(EventType ev)
     {
     case EventType::JOINING:
         PRINT_DEBUG_2("EV_JOINING");
+//        LMIC.setDrJoin(0);
         break;
     case EventType::JOINED:
         PRINT_DEBUG_2("EV_JOINED");
@@ -346,7 +347,7 @@ void powersave(OsDeltaTime maxTime)
 void loop()
 {
     OsDeltaTime to_wait = OSS.runloopOnce();
-    if (to_wait > 0 && hal_is_sleep_allow())
+    if (to_wait > OsDeltaTime(0) && hal_is_sleep_allow())
     {
         powersave(to_wait);
     }
